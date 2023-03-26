763520cc94b097bb365b5b8946bf0e4566f5684a
ofirsnb <ofirsnb@gmail.com>
2023-03-26 21:29:06 +0300

    fix

--- components/ehmtx/EHMTX.cpp
+++ components/ehmtx/EHMTX.cpp
@@ -269,7 +269,7 @@ namespace esphome
           this->display->get_text_bounds(0, 0, this->icons[i]->name.c_str(), this->font, display::TextAlign::LEFT, &x, &y, &w, &h);
           this->icon_screen->set_text(this->icons[i]->name, i, w, 1);
           ESP_LOGD(TAG, "show all icons icon: %d name: %s", i, this->icons[i]->name.c_str());
-          char* input2 = (char *) this->icons[i]->data_start;
+          char* input2 = (char *) this->icons[i]->data_start_;
           ESP_LOGD(TAG, input2);
           // ESP_LOGD(TAG, "data_start %d", this->icons[i]->data_start);
         }
